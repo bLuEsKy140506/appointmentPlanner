79a631a2ac5d0a7c0462666f277ffe923ca6dece
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2025-06-15 22:25:51 +0800

    fifth commit, adding small visual modification

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
-        {/* <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} /> */}
+        <Route index element={<Navigate to={ROUTES.CONTACTS} replace />} />
         <Route
           path={ROUTES.CONTACTS}
           element={<ContactsPage contacts={contacts} addContact={addContact} />}
--- src/components/root/Root.js
+++ src/components/root/Root.js
@@ -1,25 +1,37 @@
-import {  Outlet, NavLink } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
+import "./Root.css"; // Assuming you have a CSS file for styles
 
 export const ROUTES = {
-    CONTACTS: "/contacts",
-    APPOINTMENTS: "/appointments",
-  };
+  CONTACTS: "/contacts",
+  APPOINTMENTS: "/appointments",
+};
 
 function Root() {
-    return (
-        <>
-            <nav>
-                <NavLink to={ROUTES.CONTACTS} >
-                Contacts
-                </NavLink>
-                <NavLink to={ROUTES.APPOINTMENTS} >
-                Appointments
-                </NavLink>
-            </nav>
-            <Outlet/>
-      </>
-    );
-
+  return (
+    <>
+      <nav>
+        <NavLink
+          to={ROUTES.CONTACTS}
+          className={({ isActive }) =>
+            isActive ? "active button-48" : "button-48"
+          }
+        >
+          {" "}
+          Contacts
+        </NavLink>
+        <NavLink
+          to={ROUTES.APPOINTMENTS}
+          className={({ isActive }) =>
+            isActive ? "active button-48" : "button-48"
+          }
+        >
+          {" "}
+          Appointments
+        </NavLink>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
 
 export default Root;
